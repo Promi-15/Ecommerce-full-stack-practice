475bba025d260e492b76af07195a4d2a3bba8c92
Promi-15 <promii1217@gmail.com>
2025-07-01 00:55:09 +0600

    cart function

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -2,13 +2,15 @@ import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { ShopContext } from "../context/ShopContext";
 import { assets } from "../assets/assets";
+import RelatedProducts from "../components/RelatedProducts";
 
 const Product = () => {
   const { productId } = useParams();
   // console.log(productId)
-  const { products ,currency} = useContext(ShopContext);
+  const { products, currency ,addToCart } = useContext(ShopContext);
   const [productData, setProductData] = useState(false);
   const [image, setImage] = useState("");
+  const [size, setSize] = useState("");
 
   const fetchProductData = async () => {
     products.map((item) => {
@@ -42,23 +44,74 @@ const Product = () => {
             ))}
           </div>
           <div className="w-full sm:w[80%]">
-            <img src={image} alt="" className="w-full h-auto" />
+            <img src={image} alt="" className="w-full h-auto object-contain" />
           </div>
         </div>
         {/* product info */}
         <div className="flex-1">
           <h1 className="font-medium text-2xl mt-2">{productData.name}</h1>
           <div className="flex items-center gap-1 mt-2">
-            <img src={assets.star_icon} className="w-3.5"  />
-            <img src={assets.star_icon} className="w-3.5"  />
-            <img src={assets.star_icon} className="w-3.5"  />
-            <img src={assets.star_icon} className="w-3.5"  />
+            <img src={assets.star_icon} className="w-3.5" />
+            <img src={assets.star_icon} className="w-3.5" />
+            <img src={assets.star_icon} className="w-3.5" />
+            <img src={assets.star_icon} className="w-3.5" />
             <img src={assets.star_dull_icon} className="w-3.5" />
             <p className="pl-2">(122)</p>
           </div>
-          <p className="mt-5 text-3xl font-medium">{currency}{ productData.price}</p>
+          <p className="mt-5 text-3xl font-medium">
+            {currency}
+            {productData.price}
+          </p>
+          <p className="mt-5 text-gray-500 md:w-4/5">
+            {productData.description}
+          </p>
+          <div className="flex flex-col gap-4 my-8">
+            <p>Select Size</p>
+            <div className="flex gap-2">
+              {productData.sizes.map((item, index) => (
+                <button
+                  onClick={() => setSize(item)}
+                  key={index}
+                  className={`border py-2 px-4 bg-gray-100 ${
+                    item == size ? "border-orange-500" : ""
+                  }`}
+                >
+                  {item}{" "}
+                </button>
+              ))}
+            </div>
+          </div>
+          <button onClick={()=>addToCart(productData._id,size)} className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700">
+            Add to cart
+          </button>
+          <hr className="mt-8 sm:w-4/5" />
+          <div className="text-sm text-gray-500 mt-5 flex flex-col gap-1">
+            <p>100% Original Product</p>
+            <p>Cash on Delivery is available on this product</p>
+            <p>Easy return policy within 7 days</p>
+          </div>
         </div>
       </div>
+      {/* descriptiom and review */}
+      <div className="mt-20">
+        <div className="flex text-center">
+          <b className="border px-5 py-3 text-sm ">Description</b>
+          <p className="border px-5 py-3 text-sm">Reviews(122)</p>
+        </div>
+      </div>
+      <div className="flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500">
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores
+          praesentium nam voluptates laborum aut doloribus vero iste! Omnis, ad
+          laudantium fugiat necessitatibus voluptas nisi perferendis velit
+          doloremque, assumenda nam, deleniti delectus repellendus sunt expedita
+          debitis amet sit at tenetur? Corporis, fugiat? Expedita facilis eius
+          quaerat vero rerum, dignissimos incidunt et!
+        </p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic qui cum aliquid, quasi placeat necessitatibus nam. Sint, veritatis in vel cum tempore quam eveniet recusandae! Numquam voluptas quam voluptatem officiis?</p>
+      </div>
+      {/* related products */}
+      <RelatedProducts category={productData.category}  subCategory={productData.subCategory}/>
     </div>
   ) : (
     <div className="opacity-0"></div>
